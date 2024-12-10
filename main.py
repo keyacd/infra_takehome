98496765f90e38e2fb907bb886910ff04ae6ff96
Ubuntu <ubuntu@ip-172-31-92-66.ec2.internal>
2024-12-10 07:47:43 +0000

    improved weather readability

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import app
 import sys
 
 if __name__ == "__main__":
+    print(app.hello()[0])
     args = sys.argv[1:]
     if not isinstance(args, list):
         args = [args]
     if len(args) < 1:
         args = [input("Enter a State: ")]
     for stateabbr in args:
-        app.bird(stateabbr)
+        result = app.bird(stateabbr)
     exit()
