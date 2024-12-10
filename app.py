4fb00a43ca902672b4d45d51e9c050b7700cdb92
Ubuntu <ubuntu@ip-172-31-92-66.ec2.internal>
2024-12-10 07:35:17 +0000

    improved weather fetching, added error

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import sqlite3
 
 app = Flask(__name__)
 
+class BirdData(object):
+    def __init__(self, state=None, bird=None, scientific_name=None, year=None, abbreviation=None):
+        self.state = state
+        self.bird = bird
+        self.scientific_name = scientific_name
+        self.year = year
+        self.abbreviation = abbreviation
+    def __str__(self):
+        return "test" + self.state
 
 def get_bird(state: str):
     conn = sqlite3.connect("./birds.db")
@@ -15,14 +24,14 @@ def get_bird(state: str):
     row = cursor.execute(f"select * from birds where abbreviation = '{state}';")
     res = row.fetchall()
     list_accumulator = []
+    result = []
     for item in res:
-        print(item)
         list_accumulator.append({k: item[k] for k in item.keys()})
-    return json.dumps(list_accumulator)
-
+        result.append(BirdData(*item))
+    return json.dumps(list_accumulator), result
 
 def get_weather(state: str):
-    r = requests.get(f'https://api.weather.gov/alerts/active?area={{abbreviation}}')
+    r = requests.get(f'https://api.weather.gov/alerts/active?area={state}')
     return r.json()
 
 
@@ -35,11 +44,19 @@ def hello():
 
 @app.get('/<state>')
 def bird(state):
-    state = state.upper
-    bird = get_bird(state)
-    print(bird)
+    state = state.upper()
+    bird_json, bird_obj = get_bird(state)
+    if len(bird_obj) < 1:
+        raise ValueError(f"No birds found for abbreviation <{state}>")
+        return out, 400, {'Content-Type': 'application/json'}
+    print(f"State <{bird_obj[0].state}> has the following bird(s): ")
+    for bird in bird_obj:
+        print(f"\t{bird.bird} <{bird.scientific_name}>")
     weather = get_weather(state)
-    print(weather)
-    out = str([bird, weather])
+    if weather['title'] == 'Bad Request':
+        raise ValueError(f"Bad Request for weather")
+        return out, 400, {'Content-Type': 'application/json'}
+    print(weather['title'])
+    out = str([bird_json, weather])
     return out, 200, {'Content-Type': 'application/json'}
 
--- helm_test.sh
+++ helm_test.sh
@@ -3,4 +3,4 @@ helm uninstall birds
 echo '***RE-INSTALLING***'
 helm install birds helm/birds
 echo '***RUNNING TESTS***'
-helm test birds --hide-notes
+helm test birds --hide-notes --logs
